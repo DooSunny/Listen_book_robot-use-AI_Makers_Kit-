4d2e3c15984bdc9c86f3e5c3cad9c60163d830f8
DooSunnny <0308engo@gmail.com>
2019-04-16 20:34:50 +0900

    EOS error 해결

--- python/ex1_call_stt_tts.py
+++ python/ex1_call_stt_tts.py
@@ -21,16 +21,12 @@ def main():
 
 				if("끝" in listen_text): #잘들었어,재밌었어,다음에 또 읽어줘 
 					stt_tts.getText2VoiceStream("잘들었어. 다음에 또 읽어줘.", output_file)
+					MS.play_file(output_file)
 					break
 
 				elif("안녕" in listen_text):
 					stt_tts.getText2VoiceStream("그래그래. 인사 그만해두돼.", output_file)
-
-				else :
-					stt_tts.getText2VoiceStream("", output_file)
-				MS.play_file(output_file)
-					
-			
+					MS.play_file(output_file)
 
 		else:
 			stt_tts.getText2VoiceStream("알아들을 수 가 없습니다. 다시한번 말씀 해주세요.", output_file)
