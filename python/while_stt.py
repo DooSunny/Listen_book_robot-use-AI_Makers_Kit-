d538194b7f8f91a5cc97156712efa273dd0efbfa
DooSunny <0308engo@gmail.com>
2019-04-17 20:20:09 +0900

    구름빵 추가

--- python/while_stt.py
+++ python/while_stt.py
@@ -2,16 +2,55 @@ import stt_tts
 import MicrophoneStream as MS
 from time import sleep
 
-def stt_roop():
+def cloud_bread():#구름빵
     output_file = "testtts.wav"
     text=""
     while True:
         text = stt_tts.getVoice2Text()
         text=text.replace(" ","")
-        
-        print("text : %s"% text)
+
         if("끝" in text): #잘들었어,재밌었어,다음에 또 읽어줘 
             break
+
+        elif("내리고" in text):
+            stt_tts.getText2VoiceStream("우와", output_file)
+            MS.play_file(output_file)
+
+        elif("생길것" in text):
+            stt_tts.getText2VoiceStream("기대된다", output_file)
+            MS.play_file(output_file)
+
+        elif("작은구름" in text):
+            stt_tts.getText2VoiceStream("뭐야뭐야?", output_file)
+            MS.play_file(output_file)
+
+        elif("조심조심안고서." in text):
+            stt_tts.getText2VoiceStream("조심조심", output_file)
+            MS.play_file(output_file)
+
+        elif("동그랗게" in text):
+            stt_tts.getText2VoiceStream("음 맛있는냄새~", output_file)
+            MS.play_file(output_file)
+
+        elif("기다릴수가" in text):
+            stt_tts.getText2VoiceStream("히잉 맛있을텐데", output_file)
+            MS.play_file(output_file)
+
+        elif("배고플텐데" in text):
+            stt_tts.getText2VoiceStream("그러게요", output_file)
+            MS.play_file(output_file)
+
+        elif("맛있게잘익은" in text):
+            stt_tts.getText2VoiceStream("우와 맛있겠다아", output_file)
+            MS.play_file(output_file)
+
+        elif("아빠한테빵을" in text):
+            stt_tts.getText2VoiceStream("좋은 생각이야", output_file)
+            MS.play_file(output_file)
+
+        elif("맛있었습니다" in text):
+            stt_tts.getText2VoiceStream("맛있겠다. 나도 구름빵 먹고 싶어 넌 책을 참 잘 읽는 구나. 재밌었어 다음에 또 다른 책 읽어줘", output_file)
+            MS.play_file(output_file)
 
         elif("안녕" in text):
             stt_tts.getText2VoiceStream("그래그래. 인사 그만해두돼~~", output_file)
