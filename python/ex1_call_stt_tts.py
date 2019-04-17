95a0d804e52faa63de41de76f4b3858874a4c2aa
DooSunny <0308engo@gmail.com>
2019-04-17 20:50:45 +0900

    메인문 책추가

--- python/ex1_call_stt_tts.py
+++ python/ex1_call_stt_tts.py
@@ -18,6 +18,16 @@ def main():
 			MS.play_file(output_file)
 			while_stt.cloud_bread()
 
+		elif("호랑이" in text and (("읽을" in text) or ("읽어" in text))):
+			stt_tts.getText2VoiceStream("듣기모드를 실행할게.", output_file)
+			MS.play_file(output_file)
+			while_stt.bingle_tiger()
+
+		elif("사자 " in text and (("읽을" in text) or ("읽어" in text))):
+			stt_tts.getText2VoiceStream("듣기모드를 실행할게.", output_file)
+			MS.play_file(output_file)
+			while_stt.red_tie_lion()
+
 		else:
 			stt_tts.getText2VoiceStream("알아들을 수 가 없습니다. 다시한번 말씀 해주세요.", output_file)
 
