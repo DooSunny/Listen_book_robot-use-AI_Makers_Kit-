25e9184c3cb3a30dc6e00fb974dbbff28b43b6cb
DooSunny <0308engo@gmail.com>
2019-04-17 10:37:50 +0900

    간략화+파일추가

--- python/ex1_call_stt_tts.py
+++ python/ex1_call_stt_tts.py
@@ -1,6 +1,7 @@
 import stt_tts
 import MicrophoneStream as MS
 from time import sleep
+import while_stt
 
 def main():
 	output_file = "testtts.wav"
@@ -15,20 +16,6 @@ def main():
 		elif(("읽을" in text) or("읽어" in text)):
 			stt_tts.getText2VoiceStream("오. 재밌겠어. 듣기모드를 실행할게.", output_file)
 			MS.play_file(output_file)
-
-			while True:
-				listen_text = stt_tts.getVoice2Text()
-				listen_text=text.replace(" ","")
-
-				if("끝" in listen_text): #잘들었어,재밌었어,다음에 또 읽어줘 
-					stt_tts.getText2VoiceStream("잘들었어. 다음에 또 읽어줘.", output_file)
-					MS.play_file(output_file)
-					break
-
-				elif("안녕" in listen_text):
-					stt_tts.getText2VoiceStream("그래그래. 인사 그만해두돼.", output_file)
-					MS.play_file(output_file)
-					sleep(8)
 
 		else:
 			stt_tts.getText2VoiceStream("알아들을 수 가 없습니다. 다시한번 말씀 해주세요.", output_file)
---
+++ python/while_stt.py
@@ -0,0 +1,19 @@
+import stt_tts
+import MicrophoneStream as MS
+
+def stt_roop():
+    output_file = "testtts.wav"
+    text=""
+    while True:
+        listen_text = stt_tts.getVoice2Text()
+        listen_text=text.replace(" ","")
+
+        if("끝" in listen_text): #잘들었어,재밌었어,다음에 또 읽어줘 
+            stt_tts.getText2VoiceStream("잘들었어. 다음에 또 읽어줘.", output_file)
+            MS.play_file(output_file)
+            break
+
+        elif("안녕" in listen_text):
+            stt_tts.getText2VoiceStream("그래그래. 인사 그만해두돼.", output_file)
+            MS.play_file(output_file)
+            sleep(8)
