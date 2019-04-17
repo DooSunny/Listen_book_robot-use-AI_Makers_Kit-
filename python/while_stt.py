6a77069ce78b3da4765b1aed82c557d7d4ed3664
DooSunny <0308engo@gmail.com>
2019-04-17 20:31:48 +0900

    간결

--- python/while_stt.py
+++ python/while_stt.py
@@ -13,43 +13,43 @@ def cloud_bread():#구름빵
             break
 
         elif("내리고" in text):
-            stt_tts.getText2VoiceStream("우와", output_file)
+            stt_tts.getText2VoiceStream("우와.", output_file)
             MS.play_file(output_file)
 
         elif("생길것" in text):
-            stt_tts.getText2VoiceStream("기대된다", output_file)
+            stt_tts.getText2VoiceStream("기대된다.", output_file)
             MS.play_file(output_file)
 
         elif("작은구름" in text):
-            stt_tts.getText2VoiceStream("뭐야뭐야?", output_file)
+            stt_tts.getText2VoiceStream("뭐야뭐야?.", output_file)
             MS.play_file(output_file)
 
         elif("조심조심" in text):
-            stt_tts.getText2VoiceStream("조심조심", output_file)
+            stt_tts.getText2VoiceStream("조심조심.", output_file)
             MS.play_file(output_file)
 
         elif("동그랗게" in text):
-            stt_tts.getText2VoiceStream("음 맛있는냄새~", output_file)
+            stt_tts.getText2VoiceStream("음 맛있는냄새~.", output_file)
             MS.play_file(output_file)
 
         elif("기다릴수가" in text):
-            stt_tts.getText2VoiceStream("히잉 맛있을텐데", output_file)
+            stt_tts.getText2VoiceStream("히잉 맛있을텐데.", output_file)
             MS.play_file(output_file)
 
         elif("배고플텐데" in text):
-            stt_tts.getText2VoiceStream("그러게요", output_file)
+            stt_tts.getText2VoiceStream("그러게요.", output_file)
             MS.play_file(output_file)
 
         elif("잘익은" in text):
-            stt_tts.getText2VoiceStream("우와 맛있겠다아", output_file)
+            stt_tts.getText2VoiceStream("우와 맛있겠다아.", output_file)
             MS.play_file(output_file)
 
         elif("아빠한테" in text):
-            stt_tts.getText2VoiceStream("좋은 생각이야", output_file)
+            stt_tts.getText2VoiceStream("좋은 생각이야.", output_file)
             MS.play_file(output_file)
 
         elif("맛있었습니다" in text):
-            stt_tts.getText2VoiceStream("맛있겠다. 나도 구름빵 먹고 싶어 넌 책을 참 잘 읽는 구나. 재밌었어 다음에 또 다른 책 읽어줘", output_file)
+            stt_tts.getText2VoiceStream("맛있겠다. 나도 구름빵 먹고 싶어.", output_file)
             MS.play_file(output_file)
 
         elif("안녕" in text):
